a815b7bce5ee5f2a11408df6c96713fe18b0bc86
Zachary Hunt <=>
2019-09-07 14:38:39 -0500

    Question and Answer parser

---
+++ QAParser.py
@@ -0,0 +1,50 @@
+# Assembled dictionary where keys are categories and values are lists of lists containing (id, question, answer) triples
+questions = open("questions.txt", "r")
+
+cats = []
+line = questions.readline()
+if line != "Categories:\n":
+    print("\'Categories:\' must be the first line, followed immediately by the categories")
+
+line = questions.readline()
+
+while line != "\n":
+    cats.append(line[:-1])
+    line = questions.readline()
+
+QAs = dict([(cat, []) for cat in cats])
+
+while line == "\n":
+    line = questions.readline()
+
+if line[:-2] not in cats:
+    print("Error: Invalid Category:", line[:-2], ". Please add it to the top.")
+
+qid = 0
+while line != "":
+    cat = line[:-2]
+    line = questions.readline()
+    while line[:1] == "\t" or line[:1] == "\n":
+        if line[:1] == "\n":
+            line = questions.readline()
+            continue
+        question = line
+        answer = questions.readline()
+        try:
+            QAs[cat][qid] = [qid, question[1:-1], answer[1:-1]]
+        except IndexError:
+            QAs[cat].append([qid, question[1:-1], answer[1:-1]])
+        qid += 1
+
+        line = questions.readline()
+
+questions.close()
+
+# Outputting constructed datastructure where the reference is QAs[category][Question #][0 - id / 1 - q / 2 - a]
+# print("Categories:", cats)
+# print("Questions and Answers:\n", QAs)
+# for cat, qs in QAs.items():
+#     print(cat + ":")
+#     for q in qs:
+#         print(q[0], q[1], "\n\t", q[2])
+
